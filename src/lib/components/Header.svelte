58d24373b45afad9994adc47276f952ed6d4c341
Jerry MacPherson <jmpherso@gmail.com>
2023-10-03 13:36:16 -0500

    Mostly done header - need to add dark/light mode

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,40 +1,53 @@
-<!-- Header.svelte -->
-<header>
-	<a href="/">Home</a>
+<script>
+  // Import email, github, linkedin icon from iconoir
+  import PageIcon from '@indaco/svelte-iconoir/components/PageIcon.svelte';
+  import GitHubIcon from '@indaco/svelte-iconoir/components/GitHubIcon.svelte';
+  import LinkedInIcon from '@indaco/svelte-iconoir/components/LinkedInIcon.svelte';
+  import SunLightIcon from '@indaco/svelte-iconoir/components/SunLightIcon.svelte';
+</script>
 
-	<nav>
-		<ul>
-			<li>
-				<a href="/blog">Blog</a>
-			</li>
-			<li>
-				<a href="/about">About</a>
-			</li>
-			<li>
-				<a href="/contact">Contact</a>
-			</li>
-		</ul>
-	</nav>
+<header>
+  <div class="nav">
+	  <a class="name-link" href="https://jmpherso.github.io">Jerry MacPherson</a>
+    <div class="button-container">
+      <a href="https://github.com/jmpherso/jmpherso.github.io/blob/main/resume.pdf" class="icon-link"><PageIcon size="2xl" /></a>
+      <a href="https://github.com/jmpherso" class="icon-link"><GitHubIcon size="2xl" /></a>
+      <a href="https://www.linkedin.com/in/jerry-macpherson/" class="icon-link"><LinkedInIcon size="2xl" /></a>
+      <a href="YOUR_SUNLIGHT_LINK" class="icon-link"><SunLightIcon size="2xl" /></a>
+    </div>
+  </div>
 </header>
 
 <style>
-	header {
-		padding: 1rem;
-		background: lightskyblue;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-between;
-	}
+  header {
+    background: rgb(235, 235, 235);
+    display: flex;
+    padding: 0.5rem;
+    justify-content: center;
+    font-family: 'Young Serif', serif; /* Use the custom font */
+    font-size: 3rem;
+  }
 
-	ul {
-		margin: 0;
-		list-style-type: none;
-		display: flex;
-		gap: 1rem;
-	}
+  .nav {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    max-width: 800px;
+  }
 
-	a {
-		text-decoration: none;
-		color: inherit;
-	}
+  a.name-link, a.icon-link {
+    text-decoration: none;
+    color: inherit;
+    display: inline-block;
+    transition: transform 0.3s ease, text-shadow 0.3s ease; /* Smooth transition for hover effects */
+  }
+
+  a.name-link:hover, a.icon-link:hover {
+    transform: scale(1.2); /* Grow slightly */
+  }
+
+  .icon-link {
+    margin: 0 10px; /* Add some spacing between the icons */
+  }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,15 @@
 <!-- +layout.svelte -->
 <script>
 	import Header from '$lib/components/Header.svelte';
-  import Footer from '$lib/components/Footer.svelte';
   import '$lib/styles/mainstyle.scss';
+  import { fade } from 'svelte/transition';
+  export let data;
 </script>
 
 <Header />
 
-<main>
-	<slot />
-</main>
-
-<Footer />
+{#key data.currentRoute}
+	<main in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
+		<slot />
+	</main>
+{/key}
